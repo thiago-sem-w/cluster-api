eaacd8e3522cd7a80e172e9c396c0051a8ac3e4f
Thiago Sem W <thiagosemw@gmail.com>
2020-10-18 23:24:11 -0300

    add catch-all and error handler

--- index.js
+++ index.js
@@ -3,9 +3,22 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
 const port = process.env.PORT || 5000
+
+app.use('*', async (request, response, next) => {
+	response.status(404)
+	next(new Error('ERROR!!!'))
+})
+
+app.use(async (error, request, response, next) => {
+	const statusCode = response.statusCode || 500
+	response
+		.status(statusCode)
+		.json({ error: error.message })
+})
 
 app.listen(port, console.log(`listening on *:${ port }`))
